c0ca887dbd94cb937d91d2edb21d5cf54cd433dd
zekroTJA <riho@live.de>
2020-08-20 12:08:55 +0200

    add snowflake tests

--- test/shiori_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ShioriTest do
-  use ExUnit.Case
-  doctest Shiori
-
-  test "greets the world" do
-    assert Shiori.hello() == :world
-  end
-end
---
+++ test/snowflake_test.exs
@@ -0,0 +1,48 @@
+defmodule ShioriTest.Snowflake.Server do
+  use ExUnit.Case
+  import Shiori.Snowflake.Server
+
+  test "get snowflake ID" do
+    assert get_id(:snowflake_server) > 0
+  end
+end
+
+defmodule ShioriTest.Snowflake.Node do
+  use ExUnit.Case
+  use ExUnit.Callbacks
+
+  import Shiori.Snowflake.Server
+  import Shiori.Snowflake.Node
+
+  setup_all do
+    start_link(id: 1, name: TestSnowflakeServer.General)
+    start_link(id: 2, name: TestSnowflakeServer.NodeID)
+    start_link(id: 3, name: TestSnowflakeServer.Increments)
+
+    :ok
+  end
+
+  test "get snowflake timestamp" do
+    now = System.system_time(:millisecond)
+    id = get_id(TestSnowflakeServer.General)
+    assert get_timestamp(id) == now
+
+    now = System.system_time(:millisecond)
+    :timer.sleep(10)
+    id = get_id(TestSnowflakeServer.General)
+    assert get_timestamp(id) > now
+  end
+
+  test "get snowflake node ID" do
+    id = get_id(TestSnowflakeServer.NodeID)
+    assert get_nodeid(id) == 2
+  end
+
+  test "get snowflake increment" do
+    0..5
+    |> Enum.map(fn i -> {i, get_id(TestSnowflakeServer.Increments)} end)
+    |> Enum.each(fn {i, id} ->
+      assert get_increment(id) == i
+    end)
+  end
+end
