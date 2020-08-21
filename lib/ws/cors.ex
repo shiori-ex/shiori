abfd10fd329edb534adc4f11401b78a23505ee73
zekroTJA <riho@live.de>
2020-08-21 18:47:44 +0200

    fix cors plug

--- lib/ws/cors.ex
+++ lib/ws/cors.ex
@@ -1,26 +1,29 @@
 defmodule Shiori.WS.Cors do
-    import Plug.Conn
-    import Shiori.WS.Util
+  import Plug.Conn
+  import Shiori.WS.Util
 
-    @cors_origin Shiori.Config.get_sub(WS, :cors_origin)
+  @cors_origin Shiori.Config.get_sub(WS, :cors_origin)
 
-    def init(opts), do: opts
+  def init(opts), do: opts
 
-    def call(conn, _opts) do
-        conn = if @cors_origin != nil do
-            conn
-            |> put_resp_header("Access-Control-Allow-Origin", @cors_origin)
-            |> put_resp_header("Access-Control-Allow-Headers", "authorization, content-type, sevrer")
-            |> put_resp_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-            |> put_resp_header("Access-Control-Allow-Credentials", "true")
-        end
+  def call(conn, _opts) do
+    conn =
+      if @cors_origin != nil do
+        conn
+        |> put_resp_header("Access-Control-Allow-Origin", @cors_origin)
+        |> put_resp_header("Access-Control-Allow-Headers", "authorization, content-type, sevrer")
+        |> put_resp_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        |> put_resp_header("Access-Control-Allow-Credentials", "true")
+      else
+        conn
+      end
 
-        if conn.method == "OPTIONS" do
-            conn
-            |> resp_json_ok()
-            |> halt()
-        else
-            conn
-        end
+    if conn.method == "OPTIONS" do
+      conn
+      |> resp_json_ok()
+      |> halt()
+    else
+      conn
     end
+  end
 end
